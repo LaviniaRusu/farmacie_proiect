b23602805a2fb12bf9e36c5eb35a93f0bb8941f4
Rusu Lavinia- Teodora <laviniateodora3@gmail.com>
2025-12-10 17:07:58 +0200

    Home page - part 3

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/ui/container";
 import Link from "next/link";
 import MainNav from "./main-nav";
 import getCategories from "@/actions/get-categories";
+import NavbarActions from "./ui/navbar-actions";
 
 export const revalidate = 0;
 
@@ -15,6 +16,7 @@ const Navbar = async () => {
             <p className="font-bold text-xl">STORE</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
