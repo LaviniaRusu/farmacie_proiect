696ab7c8e31ac72945e36add8bb63fa1d03d9304
Rusu Lavinia- Teodora <laviniateodora3@gmail.com>
2025-12-12 13:12:43 +0200

    Individual product screen (Store) - > part 1

---
+++ app/(routes)/product/[productId]/page.tsx
@@ -0,0 +1,46 @@
+import getProduct from "@/actions/get-product";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+interface ProductPageProps {
+  params: Promise<{
+    productId: string;
+  }>;
+}
+
+const ProductPage = async ({ params }: ProductPageProps) => {
+  const { productId } = await params;
+  const product = await getProduct(productId);
+
+  console.log("product", product);
+
+  const suggestedProducts = await getProducts({
+    categoryId: product?.category?.id,
+  });
+
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            {/* Gallery */}
+            <div>Gallery</div>
+
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              {/* Info */}
+              <div>Info</div>
+            </div>
+          </div>
+        </div>
+        <hr className="my-10" />
+        <ProductList
+          title="Related Items"
+          items={suggestedProducts.filter((s) => s.id !== product?.id)}
+        />
+      </Container>
+    </div>
+  );
+};
+
+export default ProductPage;
